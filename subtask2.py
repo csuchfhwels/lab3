f1f1084d7055e57ddf007dbf3e8957378cb85bda
Christian Such <s2510769013@fhooe.at>
2026-01-03 14:10:42 -0500

    Add subtask1.py and subtask2.py

---
+++ subtask2.py
@@ -0,0 +1,27 @@
+n = 0
+s = 0
+
+x = int(input("Enter a number: "))
+
+while x != -1:
+    if n == 0:
+        m = x
+    else:
+        if x < m:
+            m = x
+
+    n = n + 1
+    s = s + x
+
+    x = int(input("Enter a number: "))
+
+if n == 0:
+    m = -1
+    a = -1
+else:
+    a = s / n
+
+print("n =", n)
+print("s =", s)
+print("m =", m)
+print("a =", a)
